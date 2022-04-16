278a8f648eb4617f4df68d9792a77a612e2636e2
Aliaksei Pankratau <super.ponkrat2014@yandex.ru>
2022-04-16 19:22:58 +0300

    Refactored project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { authInterceptorProviders } from './_helpers/auth.interceptor';
+import { UserpageComponent } from './components/userpage/userpage.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { authInterceptorProviders } from './_helpers/auth.interceptor';
     LoginComponent,
     RegisterComponent,
     HomeComponent,
-    ProfileComponent
+    ProfileComponent,
+    UserpageComponent
   ],
   imports: [
     FormsModule,
