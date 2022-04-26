0914c785f86cac37101555014caf79a2439a157d
Aliaksei Pankratau <super.ponkrat2014@yandex.ru>
2022-04-26 22:34:10 +0300

    Deleted debug information

--- src/app/_services/csvexport.service.ts
+++ src/app/_services/csvexport.service.ts
@@ -12,30 +12,29 @@ export class CsvexportService {
     let str = '';
     let row = 'S.No,';
     for (let index in headerList) {
-     row += headerList[index] + ',';
+      row += headerList[index] + ',';
     }
     row = row.slice(0, -1);
     str += row + '\r\n';
     for (let i = 0; i < array.length; i++) {
-     let line = (i+1)+'';
-     for (let index in headerList) {
-      let head = headerList[index];
-      line += ',' + array[i][head];
-     }
-     str += line + '\r\n';
+      let line = (i + 1) + '';
+      for (let index in headerList) {
+        let head = headerList[index];
+        line += ',' + array[i][head];
+      }
+      str += line + '\r\n';
     }
     return str;
-   }
+  }
 
-   downloadFile(data: any[], filename = 'data', headerList: any[]) {
+  downloadFile(data: any[], filename = 'data', headerList: any[]) {
     let csvData = this.ConvertToCSV(data, headerList);
-    console.log(csvData)
     let blob = new Blob(['\ufeff' + csvData], { type: 'text/csv;charset=utf-8;' });
     let dwldLink = document.createElement("a");
     let url = URL.createObjectURL(blob);
     let isSafariBrowser = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1;
     if (isSafariBrowser) {  //if Safari open in new window to save file with random filename.
-        dwldLink.setAttribute("target", "_blank");
+      dwldLink.setAttribute("target", "_blank");
     }
     dwldLink.setAttribute("href", url);
     dwldLink.setAttribute("download", filename + ".csv");
@@ -43,5 +42,5 @@ export class CsvexportService {
     document.body.appendChild(dwldLink);
     dwldLink.click();
     document.body.removeChild(dwldLink);
-}
+  }
 }
