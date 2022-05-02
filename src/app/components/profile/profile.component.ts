8cc0dbbdeaab5a05e35e4ef030535f964534341b
Aliaksei Pankratau <super.ponkrat2014@yandex.ru>
2022-05-02 15:50:40 +0300

    Created profile component

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { TokenStorageService } from 'src/app/_services/token-storage.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  storedUser: User = new User();
+
+  constructor(private tokenStorageService: TokenStorageService) { }
 
   ngOnInit(): void {
+    const user = this.tokenStorageService.getUser();
+    console.log(user);
+    this.storedUser.roleByRoleId = user.roleByRoleId[0];
+    this.storedUser.login = user.userName;
+    this.storedUser.email = user.email;
+    this.storedUser.lastName = user.lastName;
+    this.storedUser.firstName = user.firstName;
+    this.storedUser.surName = user.surName;
+    this.storedUser.phone = user.phone;
   }
-
 }
